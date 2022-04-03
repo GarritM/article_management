798e3f92ae2b964b39873c2a8410604519dd9f3c
GarritM <garrit83@gmx.de>
2022-04-03 11:07:47 +0200

    new version with dynamic memomry management and interface seperated from main

--- general_functions.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by garri on 18.03.2022.
-//
-
-#ifndef UEBUNG_PRAKTISCHE_INFORMATIK_GENERAL_FUNCTIONS_H
-#define UEBUNG_PRAKTISCHE_INFORMATIK_GENERAL_FUNCTIONS_H
-int position_in_text(char *text_part, char *text);
-#endif //UEBUNG_PRAKTISCHE_INFORMATIK_GENERAL_FUNCTIONS_H
