dbbf642fb2536b0024df691c88419448a5a3157c
GarritM <garrit83@gmx.de>
2022-05-03 11:35:27 +0200

    file paths for saving and loading can be entered

--- file_functions.c
+++ file_functions.c
@@ -7,7 +7,29 @@
 #include <malloc.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
 
+
+void file_stat(char *filepath) {
+    struct stat attr;
+    if (stat(filepath, &attr)) {
+        printf("Access to file attributes denied.\n");
+    } else {
+        if (attr.st_mode & S_IFREG) {
+            printf("regular file\n");
+        } else if (attr.st_mode & S_IFDIR) {
+            printf("directory file\n");
+        }
+        if (attr.st_mode & S_IREAD) {
+            printf("right to read\n");
+        }
+        if (attr.st_mode & S_IWRITE) {
+            printf("right to write\n");
+        }
+        printf("%ld Byte\n", attr.st_size);
+        printf("last time accessed: %ld = %s", attr.st_atime, ctime(&attr.st_atime));
+    }
+}
 struct article_type *create_article_array(int array_size){
     article_type* article = (article_type *) malloc(array_size * sizeof(article_type));
     if(article == NULL){
@@ -29,10 +51,12 @@ struct database_information_type *create_database_information(){
 struct database_type database_creation(){
     struct database_type database;
     database.file_information = create_database_information();
+    strcpy(database.file_information->file_name, "<empty>");
     database.article_array = create_article_array(1);
     database.article_array->filled = 0;
     return database;
 }
+
 void extend_article_array(struct database_type *database, int extension_size){
     database->article_array = (article_type *) realloc(database->article_array, (extension_size + database->file_information->size) * sizeof(article_type));
     if(database->article_array == NULL){
@@ -48,10 +72,16 @@ void reduce_article_array(struct database_type *database, int reduction_size){
 
 void save_database(struct database_type database){
     FILE *db_save;
-    db_save = fopen("C://Users/garri/CLionProjects/Gittesting/Database.csv", "wb");
+    if(strcmp(database.file_information->file_name, "<empty>") == 0){
+        printf("Where do u want to save this file? Type in a path:\n");
+        scanf("%s",database.file_information->file_name); //TODO: filename, should be assembled so that the file is saved in the same place always
+                                                          //      this makes it possible to display the files from which u can load
+                                                          //TODO: more general, saving and loading should work not only on this computer
+    }
+    db_save = fopen(database.file_information->file_name, "wb");
     /*fehlerprüfung*/
     if(db_save == NULL){
-        printf("file error occurred");
+        printf("file error occurred\n");
     }
         /*tatsächliches schreiben in datei*/
     else{
@@ -70,9 +100,11 @@ void save_database(struct database_type database){
     fclose(db_save);
 }
 void load_database(struct database_type *database){
-        FILE *db_read;
-        char line_buffer[500];
-        if((db_read=fopen("C://Users/garri/CLionProjects/Gittesting/Database.csv", "r")) == NULL){
+    FILE *db_read;
+    char line_buffer[500], file_name[1000];
+    printf("Path of file to open:\n");
+    scanf("%s", &database->file_information->file_name);
+    if((db_read=fopen(database->file_information->file_name, "r")) == NULL){
             printf("loading unsuccessful\n");
         }else{
             fscanf(db_read, "%s", line_buffer);
@@ -88,8 +120,10 @@ void load_database(struct database_type *database){
                 database->article_array[i].price_c = atof(strtok(NULL,";"));
                 database->article_array[i].filled = atof(strtok(NULL,";"));
             }
+            strcpy(database->file_information->file_name,file_name);
             printf("loading successful\n");
+
         }
-        fclose(db_read);
+    fclose(db_read);
 }
 
