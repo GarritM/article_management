005c2167c9820194b58d2b494fdfc36b33ac03ad
GarritM <garrit83@gmx.de>
2022-07-07 18:08:45 +0200

    network- and filefunctions.c now closer to work on UNIX... there are still some problems

--- network_functions.h
+++ network_functions.h
@@ -4,11 +4,11 @@
 
 #ifndef ARTICLE_MANAGEMENT_1_2_NETWORK_FUNCTIONS_H
 #define ARTICLE_MANAGEMENT_1_2_NETWORK_FUNCTIONS_H
-#include <winsock2.h>
 #ifdef _WIN32
+#include <winsock2.h>
 #define socket_type SOCKET
 #else
-#define socket int
+#define socket_type int
 #endif
 /*evaluates error, returns error message*/
 int exit_error(char *error_message);
@@ -16,12 +16,12 @@ int exit_error(char *error_message);
 int init_winsock();
 #endif
 socket_type create_socket();
-void bind_socket(SOCKET *sock, unsigned long address, unsigned short port);
-void listen_socket(SOCKET *socket);
-void accept_socket(SOCKET *socket, SOCKET *new_socket);
-void connect_socket(SOCKET *socket, char *serv_addr, unsigned short port);
-void TCP_send(SOCKET *socket, char *data, size_t size);
-void TCP_receive(SOCKET *socket, char *data, size_t size);
+void bind_socket(socket_type *sock, unsigned long address, unsigned short port);
+void listen_socket(socket_type *socket);
+void accept_socket(socket_type *socket, socket_type *new_socket);
+void connect_socket(socket_type *socket, char *serv_addr, unsigned short port);
+void TCP_send(socket_type *socket, char *data, size_t size);
+void TCP_receive(socket_type *socket, char *data, size_t size);
 void cleanup(void);
 
 int init_server();
